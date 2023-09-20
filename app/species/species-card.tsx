2c864b5c997fcd8e1dfa4916e46accb94f86d82c
Alexis Hu <alexishu@dhcp-10-250-190-70.harvard.edu>
2023-09-20 13:28:15 -0400

    added Delete Species Button

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -1,12 +1,20 @@
-// import { Button } from "@/components/ui/button";
+"use client";
+
+import { Icons } from "@/components/icons";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
 
 import EditSpeciesDialog from "./edit-species-dialog";
 import MoreInfoDialog from "./more-info-dialog";
 
+import { toast } from "@/components/ui/use-toast";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
+import { Button } from "@/components/ui/button";
+// import console from "console";
 import React from "react";
 
 interface SpeciesCardProps {
@@ -16,11 +24,45 @@ interface SpeciesCardProps {
 }
 
 const SpeciesCard: React.FC<SpeciesCardProps> = ({ userId, species }) => {
+  const router = useRouter();
+
+  const onDelete = () => {
+    (async () => {
+      const supabase = createClientComponentClient<Database>();
+      try {
+        await supabase.from("species").delete().eq("id", species.id);
+      } catch {
+        // console.error("An error has occurred", err);
+        toast({
+          title: "delete result: {$result}.",
+          variant: "destructive",
+        });
+      }
+
+      router.refresh();
+    })().catch((err) =>
+      toast({
+        title: `Something went wrong: ${err}`,
+        variant: "destructive",
+      }),
+    );
+  };
+
   const renderOption = () => {
     if (userId != species.author) {
       return <MoreInfoDialog key={species.id} {...species} />;
     } else {
-      return <EditSpeciesDialog key={species.id} species={species} />;
+      return (
+        <div>
+          <div>
+            <EditSpeciesDialog key={species.id} species={species} />
+          </div>
+          <Button variant="secondary" onClick={onDelete}>
+            <Icons.trash className="mr-3 h-5 w-5" />
+            Delete Species
+          </Button>
+        </div>
+      );
     }
   };
 
